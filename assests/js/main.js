f26314af3c1f2c7e2d92037ee2d8f43582e45c0a
letranbason <109140737+letranbason@users.noreply.github.com>
2023-04-29 08:38:43 +0700

    update 29/4

--- assests/js/main.js
+++ assests/js/main.js
@@ -6,3 +6,36 @@ $(function() {
 		$('.overlay').toggleClass('visible');
 	});
 });
+
+const image = document.getElementById('my-image');
+const zoomInButton = document.getElementById('zoom-in-button');
+const zoomOutButton = document.getElementById('zoom-out-button');
+
+let scale = 1;
+
+zoomInButton.addEventListener('click', function() {
+  scale += 0.1;
+  image.style.transform = `scale(${scale})`;
+});
+
+zoomOutButton.addEventListener('click', function() {
+  scale -= 0.1;
+  if (scale <= 0) {
+    scale = 0;
+  }
+  image.style.transform = `scale(${scale})`;
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === '+') {
+    scale += 0.1;
+    image.style.transform = `scale(${scale})`;
+  }
+  if (event.key === '-') {
+    scale -= 0.1;
+    if (scale <= 0) {
+      scale = 0;
+    }
+    image.style.transform = `scale(${scale})`;
+  }
+});
